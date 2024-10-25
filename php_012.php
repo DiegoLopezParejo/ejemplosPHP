fa028b998eb4e62c2c9c9232963e9340ee4e1523
Diego Lopez <diego.lopezparejo@riberadeltajo.es>
2024-10-25 09:31:25 +0200

    hasta el 14.2

--- php_012.php
+++ php_012.php
@@ -1,11 +1,18 @@
 <?php
-    // funcion isset
-    // Comprueba si una variable está configurada, es decir, esta declarada y no es NULL. En este caso devuelve 1
-    // En caso contrario devuelve 0
+    // funcion isset Devuelve 1 si está definida la variable. 0 en caso contrario
+    // Comprueba si una variable está definida y no es NULL
+    //El valor NULL representa una variable SIN VALOR
 $var1 = 4;
 if(isset($var1)){
     print("La variable var1 esta inicializada y no es null. Su valor es:  $var1");
 }else{
     print("La variable var1 no esta inicializada");
+}
+echo "<br>";
+$var2 = null;
+if(isset($var2)){
+    print("La variable var1 esta inicializada y no es null. Su valor es:  $var2");
+}else{
+    print("La variable var2 no esta defnida y su valor es NULL");
 }
 ?>
--- php_013.php
+++ php_013.php
@@ -2,15 +2,15 @@
     //unset:Elimina una variable o array respetando el ámbito de dicha variable.
     $var1 = 4;
     if(isset($var1)){
-        print("La variable var1 esta inicializada y no es null. Su valor es:  $var1");
+        print("La variable var1 esta definida y no es null. Su valor es:  $var1");
     }else{
-        print("La variable var1 no esta inicializada");
+        print("La variable var1 no esta definida");
     }
     unset($var1);//elimino la variable
     echo "<br>";
     if(isset($var1)){ 
-        print("La variable var1 esta inicializada y no es null. Su valor es:  $var1");
+        print("La variable var1 esta definida y no es null. Su valor es:  $var1");
     }else{
-        print("La variable var1 no esta inicializada");
+        print("La variable var1 no esta definida");
     }
 ?>""
